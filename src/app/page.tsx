de77743102bac3b0846aed1717c2acae39015392
Bhavin Prajapati <bhavin.prajapati@gmail.com>
2026-05-12 11:35:00 -0400

    Scroll bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeScreen from './components/HomeScreen';
 
 export default function Home() {
   return (
-    <main className="h-screen overflow-hidden">
+    <main className="h-screen overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-stone-100 [&::-webkit-scrollbar-thumb]:bg-stone-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
       <HomeScreen />
     </main>
   );
